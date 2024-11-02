84d34e75eb426b989d91de846e60205dcedd97f5
Sachini <sachichathudissanayaka@gmail.com>
2024-11-02 21:49:03 +0530

    Add changes

--- config/logger.js
+++ config/logger.js
@@ -1,10 +1,24 @@
 import winston from "winston";
 
+const colors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  http: 'magenta',
+  debug: 'cyan',
+};
+
+winston.addColors(colors);
+
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.combine(
+    winston.format.colorize(),
     winston.format.timestamp(),
-    winston.format.json()
+    winston.format.json(),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} ${level}: ${message}`; 
+    })
   ),
   transports: [new winston.transports.Console()],
 });
