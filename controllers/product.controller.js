78587d0c569df2df78e4365977a1a2677a61e889
Sachini <sachichathudissanayaka@gmail.com>
2024-11-02 23:58:21 +0530

    Add changes

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -5,10 +5,10 @@ import logger from "../config/logger.js";
 export const getProducts = async (req, res) => {
   try {
     const products = await Product.find({});
-    logger.info('Fetched products successfully');
+    logger.info("Fetched products successfully");
     res.status(200).json(products);
   } catch (error) {
-    logger.error('Error fetching products: ' + error.message);
+    logger.error("Error fetching products");
     res.status(500).json({ message: error.message });
   }
 };
@@ -18,10 +18,10 @@ export const getProduct = async (req, res) => {
   try {
     const { id } = req.params;
     const product = await Product.findById(id);
-    logger.info('Fetched product successfully');
+    logger.info("Fetched product successfully");
     res.status(200).json(product);
   } catch (error) {
-    logger.error('Error fetching product: ' + error.message);
+    logger.error("Error fetching product");
     res.status(500).json({ message: error.message });
   }
 };
@@ -29,11 +29,24 @@ export const getProduct = async (req, res) => {
 // Add a new product
 export const createProduct = async (req, res) => {
   try {
-    const product = await Product.create(req.body);
-    logger.info('Added product successfully');
+    if (!req.body.name || !req.body.quantity || !req.body.price) {
+      logger.error("Missing one or more required fields");
+      return res.status(400).send({
+        message: "Missing one or more required fields",
+      });
+    }
+
+    const newProduct = {
+      name: req.body.name,
+      quantity: req.body.quantity,
+      price: req.body.price,
+    };
+
+    const product = await Product.create(newProduct);
+    logger.info("Added product successfully");
     res.status(200).json(product);
   } catch (error) {
-    logger.error('Error adding product: ' + error.message);
+    logger.error("Error adding product");
     res.status(500).json({ message: error.message });
   }
 };
@@ -44,14 +57,14 @@ export const updateProduct = async (req, res) => {
     const { id } = req.params;
     const product = await Product.findByIdAndUpdate(id, req.body);
     if (!product) {
-      logger.error('Error fetching product: ' + error.message);
+      logger.error("Error fetching product");
       return res.status(404).json({ message: "Product not found" });
     }
     const updatedProduct = await Product.findById(id);
-    logger.info('Updated product successfully');
+    logger.info("Updated product successfully");
     res.status(200).json(updatedProduct);
   } catch (error) {
-    logger.error('Error updating product: ' + error.message);
+    logger.error("Error updating product");
     res.status(500).json({ message: error.message });
   }
 };
@@ -62,13 +75,13 @@ export const deleteProduct = async (req, res) => {
     const { id } = req.params;
     const product = await Product.findByIdAndDelete(id);
     if (!product) {
-      logger.error('Error fetching product: ' + error.message);
+      logger.error("Error fetching product");
       return res.status(404).json({ message: "Product not found" });
     }
-    logger.info('Deleted product successfully');
+    logger.info("Deleted product successfully");
     res.status(200).json({ message: "Product deleted successfully." });
   } catch (error) {
-    logger.error('Error deleting product: ' + error.message);
+    logger.error("Error deleting product");
     res.status(500).json({ message: error.message });
   }
 };
